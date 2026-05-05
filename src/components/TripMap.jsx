808da8415479bbe3a8346a2d86b42d30aa812589
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2026-05-05 12:32:12 -0400

    Refactor TripMap component to enhance event marker interactivity and styling

--- src/components/TripMap.jsx
+++ src/components/TripMap.jsx
@@ -13,19 +13,33 @@ export default function TripMap({ events }) {
       style={{ height: '100%', width: '100%', border: '1px solid black' }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {events.map(event => (
-        <CircleMarker
-          center={event.extendedProps.coords}
-          radius={8}
-          pathOptions={
-            event.isConfirmed
-              ? { color: '#E24B4A', opacity: 0.85, fillColor: '#E24B4A', fillOpacity: 0.85 }
-              : { color: '#808080', opacity: 0.65, fillColor: '#808080', fillOpacity: 0.65 }
-          }
-        >
-          <Tooltip>{event.title}</Tooltip>
-        </CircleMarker>
-      ))}
+      {events.map(event => {
+        const baseOptions = event.isConfirmed
+          ? { color: 'rgba(213, 0, 0, 0.65)', opacity: 0.65, fillColor: 'rgba(213, 0, 0, 0.65)', fillOpacity: 0.65, weight: 1}
+          : { color: '#808080', opacity: 0.65, fillColor: '#808080', fillOpacity: 0.65, weight: 1 }
+        return (
+          <CircleMarker
+            key={event.id}
+            center={event.extendedProps.coords}
+            radius={4}
+            pathOptions={baseOptions}
+            eventHandlers={{
+              mouseover: e => { 
+                const hoverColor = event.isConfirmed ? 'rgba(160, 0, 0, 0.85)' : '#4a4a4a'; 
+                e.target.setRadius(8); 
+                e.target.setStyle({ color: hoverColor, fillColor: hoverColor, opacity: 1, fillOpacity: 1, weight: 1}) },
+              mouseout:  e => { 
+                e.target.setRadius(4); 
+                e.target.setStyle({ color: baseOptions.color, fillColor: baseOptions.fillColor, opacity: baseOptions.opacity, fillOpacity: baseOptions.fillOpacity, weight: baseOptions.weight }) },
+              // click: e => {
+
+              // }
+            }}
+          >
+            <Tooltip>{event.title}</Tooltip>
+          </CircleMarker>
+        )
+      })}
     </MapContainer>
   )
 }
